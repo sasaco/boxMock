ddca57a288241636c7d0442f628cb6ae1e6322a0
haruka0319 <haruharu.0319_morimori@docomo.ne.jp>
2020-10-17 15:22:58 +0900

    page1-6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { Page5Component } from './page5/page5.component';
+import { Page6Component } from './page6/page6.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page4Component,
+    Page5Component,
+    Page6Component
   ],
   imports: [
     BrowserModule,
